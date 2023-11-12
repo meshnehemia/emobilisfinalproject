140cb161fa0a727fc2370aa556b6af581a7b5896
meshnehemia <meshnehemia7@gmail.com>
2023-11-13 02:13:49 +0300

    still continuation

--- socialmedia/views.py
+++ socialmedia/views.py
@@ -63,7 +63,7 @@ def home(request):
         Q(name__icontains =q)   |
         Q(description__icontains =q)
         )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages =Message.objects.filter(Q(room__topic__name__icontains = q))
     context = {'rooms':rooms,'topics':topics,'room_count':room_count,"room_messages":room_messages}
@@ -163,3 +163,9 @@ def updateUser(request):
             form.save()
             return redirect('user-profile',pk=user.id)
     return render(request,'socialmedia/update-user.html',context)
+
+def  topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics =Topic.objects.filter(name__icontains=q)
+    context ={'topics':topics}
+    return render(request,'socialmedia/topics.html',context)
